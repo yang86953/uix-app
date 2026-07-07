2484d42bf2cdc85983202b3d127d1afe0d5a200e
yang86 <869532085@qq.com>
2026-07-07 08:51:13 +0800

    接入 Tooltip 过渡动画

---
+++ src/tests/ui/widgets/feedback/tooltip.rs
@@ -0,0 +1,32 @@
+use crate::ui::traits::WidgetAnimation;
+use crate::ui::widgets::feedback::Tooltip;
+
+#[test]
+fn tooltip_enter_animation_finishes_visible() {
+    let mut tooltip = Tooltip::new("Help");
+
+    tooltip.open();
+    assert!(tooltip.is_visible());
+    assert!(tooltip.is_present());
+
+    assert!(WidgetAnimation::update_animation(&mut tooltip, 0.05));
+    assert!(!WidgetAnimation::update_animation(&mut tooltip, 1.0));
+    assert!(tooltip.is_visible());
+    assert!(tooltip.is_present());
+}
+
+#[test]
+fn tooltip_exit_animation_stays_present_until_finished() {
+    let mut tooltip = Tooltip::new("Help");
+    tooltip.open();
+    assert!(!WidgetAnimation::update_animation(&mut tooltip, 1.0));
+
+    tooltip.close();
+    assert!(!tooltip.is_visible());
+    assert!(tooltip.is_present());
+
+    assert!(WidgetAnimation::update_animation(&mut tooltip, 0.03));
+    assert!(!WidgetAnimation::update_animation(&mut tooltip, 1.0));
+    assert!(!tooltip.is_visible());
+    assert!(!tooltip.is_present());
+}
