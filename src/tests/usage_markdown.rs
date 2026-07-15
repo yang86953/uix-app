a2b4ee1756b0c54d05d4fe0fc84ee89d0efb6869
yang86 <869532085@qq.com>
2026-07-15 16:42:41 +0800

    docs(form): compile validation examples

--- src/tests/usage_markdown.rs
+++ src/tests/usage_markdown.rs
@@ -3,5 +3,5 @@ include!(concat!(env!("OUT_DIR"), "/usage_markdown_compile.rs"));
 #[test]
 fn usage_markdown_compile_inventory_is_explicit() {
     assert_eq!(USAGE_RUST_BLOCKS_TOTAL, 98);
-    assert_eq!(USAGE_RUST_BLOCKS_COMPILED, 53);
+    assert_eq!(USAGE_RUST_BLOCKS_COMPILED, 57);
 }
