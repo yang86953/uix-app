69e1c9b33c379d8c4aec6fdde91d35e295058cfa
yang86 <869532085@qq.com>
2026-07-15 17:43:12 +0800

    docs(config): publish token and empty view contracts

--- src/tests/usage_markdown.rs
+++ src/tests/usage_markdown.rs
@@ -2,6 +2,6 @@ include!(concat!(env!("OUT_DIR"), "/usage_markdown_compile.rs"));
 
 #[test]
 fn usage_markdown_compile_inventory_is_explicit() {
-    assert_eq!(USAGE_RUST_BLOCKS_TOTAL, 98);
-    assert_eq!(USAGE_RUST_BLOCKS_COMPILED, 98);
+    assert_eq!(USAGE_RUST_BLOCKS_TOTAL, 99);
+    assert_eq!(USAGE_RUST_BLOCKS_COMPILED, 99);
 }
